f4692ce91a8798a9dc58c58bd1ef50886b7c8550
kenedfelix <kened.felix@gmail.com>
2022-07-03 21:38:13 -0300

    Criado os compenents de Input/Select/Button

---
+++ code/src/components/Button.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export default styled.button`
+    width: 100%;
+    height:52px;
+    border:none;
+    background:#5061FC;
+    font-size:16px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    font-weight: bold;
+    color:#fff;
+    border-radius: 4px;
+    transition:background 0.2s ease-in;
+
+    &:hover{
+        background: #6674F4;
+
+    }
+
+    &:active{
+        background: #3346F0;
+    }
+    &[disabled]{
+        background:#ccc;
+        cursor:default;
+    }
+`;
--- code/src/pages/NewContact/index.js
+++ code/src/pages/NewContact/index.js
@@ -1,9 +1,19 @@
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
+import Select from "../../components/Select";
 
 export default function NewContact(){
     return (
+        <>
         <PageHeader
             title="Novo Contato"
         />
+        <Input type="text" placeholder="Nome"></Input>
+        <Select >
+            <option value="123">Instagram</option>
+        </Select>
+        <Button type="Button" disabled>Salvar Alterações</Button>
+        </>
     );
 }
